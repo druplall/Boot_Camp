2ad4af025174f9cc03750781d9f68aeb685a9c91
Deodat Ruplall <druplall@hunter.cuny.edu>
2019-03-04 17:46:28 -0500

    Adding Functions and statement test results

--- Function.py
+++ Function.py
@@ -20,3 +20,26 @@ def dog_check(mystring):
     return 'dog' in mystring.lower()
 
 print(dog_check('MY DOG is great !'))
+
+# Pig Latin code
+def pig_latin (s):
+    mylist = ['a','e','i','o','u']
+    for i in mylist:
+        if s[0].lower() == i:
+            return (s + 'ay')
+        else:
+            return (s[1:] + s[0] + 'ay')
+
+test = pig_latin('apple')
+print(test)
+
+def pig_latin_two (word):
+    first_letter = word[0]
+
+    if first_letter in 'aeiou':
+        pig_word = word + 'ay'
+    else:
+        pig_word = word[1:] + first_letter + 'ay'
+    return pig_word
+
+print(pig_latin_two('a'))
